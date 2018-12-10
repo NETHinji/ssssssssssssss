726d143e504c4789d2b7760df4c2c68696ff7272
NETHinji <44230892+NETHinji@users.noreply.github.com>
2018-12-10 13:40:18 +0530

    Add files via upload

---
+++ Mini/Mini/Controllers/TicketDetailsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Mini.Models;
+
+namespace Mini.Controllers
+{
+    public class TicketDetailsController : ApiController
+    {
+        private Training_19Sep18_PuneEntities1 db = new Training_19Sep18_PuneEntities1();
+
+        // GET: api/TicketDetails
+        public IQueryable<TicketDetails> GetTicketDetails()
+        {
+            return db.TicketDetails;
+        }
+
+        // GET: api/TicketDetails/5
+        [ResponseType(typeof(TicketDetails))]
+        public IHttpActionResult GetTicketDetail(int id)
+        {
+            TicketDetails ticketDetail = db.TicketDetails.Find(id);
+            if (ticketDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ticketDetail);
+        }
+
+        // PUT: api/TicketDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(int id, TicketDetails t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != t.ReqID)
+            {
+                return BadRequest();
+            }
+
+            //db.Entry(t).State = EntityState.Modified;
+            db.TicketAction(id,t.managerApproval);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TicketDetailExists(int id)
+        {
+            return db.TicketDetails.Count(e => e.ReqID == id) > 0;
+        }
+    }
+}
---
+++ Mini/Mini/Controllers/TicketDetailsMVCController.cs
@@ -0,0 +1,27 @@
+﻿using Mini.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mini.Controllers
+{
+    public class TicketDetailsMVCController : Controller
+    {
+        // GET: TicketDetailsMVC
+        public ActionResult Display()
+        {
+           
+            return View();
+            
+        }
+        public ActionResult Update()
+        {
+
+            return View();
+        }
+
+    }
+}
